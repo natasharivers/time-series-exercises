48ed5a7e9723fd2ee32cc2b3c515c03fb5f7c0ea
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-07-01 11:12:18 -0500

    add functions

--- acquire.py
+++ acquire.py
@@ -115,9 +115,41 @@ def sales_df():
         
     return sales
 
+#################################################################
+
+######################### Helper function used in create big_df ########################################
+
+def get_df(name):
+    """
+    This function takes in the string
+    'items', 'stores', or 'sales' and
+    returns a df containing all pages and
+    creates a .csv file for future use.
+    """
+    base_url = 'https://python.zach.lol'
+    api_url = base_url + '/api/v1/'
+    response = requests.get(api_url + name)
+    data = response.json()
+    
+    # create list from 1st page
+    my_list = data['payload'][name]
+    
+    # loop through the pages and add to list
+    while data['payload']['next_page'] != None:
+        response = requests.get(base_url + data['payload']['next_page'])
+        data = response.json()
+        my_list.extend(data['payload'][name])
+    
+    # Create DataFrame from list
+    df = pd.DataFrame(my_list)
+    
+    # Write DataFrame to csv file for future use
+    df.to_csv(name + '.csv')
+    return df
+
 ############################# MERGE DATA FUNCTION #########################
 
-def get_store_data():
+def merged_data():
     '''
     This function merges items, stores, and sales dataframes
     and returns the completed merged df
@@ -138,3 +170,40 @@ def get_store_data():
     complete_df = pd.merge(merged_df, stores, on="store")
 
     return completed_df
+
+############################# STORE DATA FUNCTION #########################
+
+def get_store_data():
+    """
+    This function checks for csv files
+    for items, sales, stores, and big_df 
+    if there are none, it creates them.
+    It returns one big_df of merged dfs.
+    """
+    # check for csv files or create them
+    if os.path.isfile('items.csv'):
+        items_df = pd.read_csv('items.csv', index_col=0)
+    else:
+        items_df = get_df('items')
+        
+    if os.path.isfile('stores.csv'):
+        stores_df = pd.read_csv('stores.csv', index_col=0)
+    else:
+        stores_df = get_df('stores')
+        
+    if os.path.isfile('sales.csv'):
+        sales_df = pd.read_csv('sales.csv', index_col=0)
+    else:
+        sales_df = get_df('sales')
+        
+    if os.path.isfile('big_df.csv'):
+        df = pd.read_csv('big_df.csv', index_col=0)
+        return df
+    else:
+        # merge all of the DataFrames into one
+        df = pd.merge(sales_df, stores_df, left_on='store', right_on='store_id').drop(columns={'store'})
+        df = pd.merge(df, items_df, left_on='item', right_on='item_id').drop(columns={'item'})
+
+        # write merged DateTime df with all data to directory for future use
+        df.to_csv('big_df.csv')
+        return df
